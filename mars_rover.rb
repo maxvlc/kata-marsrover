4d00c638ac0f1ca91752f2f98d12f45d2a6ed4a9
maxvlc <maxcar@gmail.com>
2014-07-15 16:17:35 +0200

    Implement commands that move the rover forward/backward (f,b) left/right GREEN

--- mars_rover.rb
+++ mars_rover.rb
@@ -1,11 +1,33 @@
 class MarsRover
 
-	def ready (pos,dir)
-		"Expecting order"
+	DEFAULT_POSITION = [0,0]
+	DEFAULT_ORIENTATION = :N
+	STEP = 1
+	@rover_position
+
+
+	def initialize()
+		@rover_position = DEFAULT_POSITION
+		@rover_orientation = DEFAULT_ORIENTATION
 	end
 
-	def order (commands)
-		"Order Received"
+	def where_are_you
+		@rover_position
 	end
 
+	def where_is_your_orientation
+		@rover_orientation
+	end
+
+	def move(order)
+		@rover_position[0]+=STEP if order == "f"
+		@rover_position[0]-=STEP if order == "b"
+		@rover_position
+	end
+
+	def turn (order)
+		@rover_position[1]+=STEP if order == "l"
+		@rover_position[1]-=STEP if order == "r"
+		@rover_position
+	end
 end
--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -1,21 +1,24 @@
 require './mars_rover'
 
 describe "The Mars Rover" do
-	x = 0
-	y = 0
-	direction = "N"
-	position = [x,y]
-	commands =[]
+
+	DEFAULT_POSITION = [0,0]
+	DEFAULT_DIRECTION = :N
 
 	it "has an initial starting point and a direction" do
-		expect(MarsRover.new.ready(position,direction)).to eq ("Expecting order")
+		expect(MarsRover.new.where_are_you).to eq DEFAULT_POSITION
+		expect(MarsRover.new.where_is_your_orientation).to eq DEFAULT_DIRECTION
 	end
 
-	it "receives a character array of commands" do
-		expect(MarsRover.new.order(commands)).to eq ("Order Received")
+	it "moves 1 unit with an f command and -1 with b" do
+		final_position = [1,0]
+		expect(MarsRover.new.move("f")).to eq (final_position)
+		expect(MarsRover.new.move("b")).to eq DEFAULT_POSITION
 	end
 
-	it "Implement commands that move the rover forward/backward (f,b)." do
-		expect(MarsRover.new.order(commands)).to eq("f","b")
+	it "moves 1 unit to left with an l command and move right with r" do
+		final_position = [0,1]
+		expect(MarsRover.new.turn("l")).to eq (final_position)
+		expect(MarsRover.new.turn("r")).to eq DEFAULT_POSITION
 	end
 end
